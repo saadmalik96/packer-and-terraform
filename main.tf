ed9b468dc4c68ec6495a7313f98f15c13971d9e0
Saad Malik Paracha <35294802+saadmalik96@users.noreply.github.com>
2025-03-27 17:24:01 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,13 +37,19 @@ module "private_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "private-instance-sg"
-  description = "Allow SSH from bastion"
+  description = "Allow SSH from bastion + intra-group access"
   vpc_id      = module.vpc.vpc_id
 
   ingress_with_source_security_group_id = [{
     rule                     = "ssh-tcp"
     source_security_group_id = module.bastion_sg.security_group_id
   }]
+
+  ingress_with_self = [
+    {
+      rule = "all-all"
+    }
+  ]
 
   egress_rules = ["all-all"]
 }
